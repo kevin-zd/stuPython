b435fabef80f48d0fa715bb23da6a2a106516cd4
zhaodding <ban_bu@163.com>
2023-08-27 22:25:50 +0800

    条件判断语句

--- 01-基础知识/13-条件判断语句.py
+++ 01-基础知识/13-条件判断语句.py
@@ -13,3 +13,14 @@ if age > 18:
     print("可以进入网吧")
 else:
     print("go out")
+
+''' if 做判断 elif'''
+age = int(input("请输入你的年龄："))
+if age < 18:
+    print(f'你输入的年龄是{age}，童工')
+elif(age >= 18) and (age <= 60):
+    print(f'你输入的年龄是{age}，合法')
+elif age > 60:
+    print(f'你输入的年龄是{age}，退休年龄')
+
+
