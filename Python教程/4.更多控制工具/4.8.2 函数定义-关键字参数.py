ecfb99b83d0d38ad210831e2578dae28f41c07fa
zhaodding <ban_bu@163.com>
2024-01-18 21:58:28 +0800

    python教程之函数定义及未进行版本管理的文件

--- Python教程/4.更多控制工具/4.8.2 函数定义-关键字参数.py
+++ Python教程/4.更多控制工具/4.8.2 函数定义-关键字参数.py
@@ -5,6 +5,7 @@
     @date：2024/1/18 21:48
     @Desc: 
 """
+""" 语法一： """
 def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
     print("-- This parrot wouldn't", action, end=' ')
     print("if you put", voltage, "volts through it.")
@@ -17,3 +18,20 @@ parrot(voltage=1000000, action='VOOOOOM')  # 2 keyword arguments
 parrot(action='VOOOOOM', voltage=1000000)  # 2 keyword arguments
 parrot('a million', 'bereft of life', 'jump')  # 3 positional arguments
 parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+
+""" 语法二： """
+print("---------- 语法二 --------------")
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
